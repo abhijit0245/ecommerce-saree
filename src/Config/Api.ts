8b00498c7b587acff1a3e11b2102961f9d590ab5
abhijit0245 <papu09353@gmail.com>
2025-05-05 17:29:52 +0530

    finsh

--- src/Config/Api.ts
+++ src/Config/Api.ts
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
-export const API_URL = "http://localhost:5454";
-export const DEPLOYED_URL = "https://7b5c-2406-7400-70-ed22-501-65d2-2eb5-15fc.ngrok-free.app/"
-// change api
+// Localhost (for development)
+// export const API_URL = "http://localhost:5454";
+
+// Render (for deployed backend)
+// export const API_URL = "https://zosh-bazzar-backend.onrender.com";
+
+// Ngrok (for demo or temporary public access)
+export const API_URL = "https://59d0-2406-7400-70-ed22-501-65d2-2eb5-15fc.ngrok-free.app";
 
 export const api = axios.create({
   baseURL: API_URL, 
